2d67733570f5c64b16fef7798e0a8d82d0997726
AnandYellapu <anandsaiii1200@gmail.com>
2023-07-23 12:24:50 +0530

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
