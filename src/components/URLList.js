e72ac0e0a1a0d177846646eb831d97810cc38c98
Yellapu AnandSai <104178105+AnandYellapu@users.noreply.github.com>
2023-07-31 08:12:03 +0530

    Update URLList.js

--- src/components/URLList.js
+++ src/components/URLList.js
@@ -1,6 +1,140 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import useUpdateClickCount from './useUpdateClickCount'; // Adjust the path based on your project structure
+
+// const URLList = () => {
+//   const [urlList, setUrlList] = useState([]);
+
+//   useEffect(() => {
+//     const fetchUrlList = async () => {
+//       try {
+//         const response = await axios.get('https://url-shortener-ax8r.onrender.com/api/url-list');
+//         setUrlList(response.data);
+//       } catch (error) {
+//         console.error('Error fetching URL list:', error);
+//         // Handle error cases
+//       }
+//     };
+
+//     fetchUrlList();
+//   }, []);
+
+//   const { clickCount, handleUpdateClickCount } = useUpdateClickCount();
+
+//   const handleShortUrlClick = async (event, urlId, index) => {
+//     event.preventDefault();
+//     handleUpdateClickCount(urlId);
+//     // You can also handle the URL opening here if needed
+//   };
+
+//   return (
+//     <div className="url-list-container">
+//       <h2 className="url-list-heading">URL List</h2>
+//       <table className="url-list-table">
+//         <thead>
+//           <tr>
+//             <th>Short URL</th>
+//             <th>Long URL</th>
+//             <th>Click Count</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {urlList.map((url, index) => (
+//             <tr key={index}>
+//               <td>
+//                 <a href={url.shortURL} target="_blank" rel="noopener noreferrer" onClick={(e) => handleShortUrlClick(e, url._id, index)}>
+//                   {url.shortURL}
+//                 </a>
+//               </td>
+//               <td>
+//                 <div className="long-url-container">
+//                   <span className="long-url-text">{url.longURL}</span>
+//                 </div>
+//               </td>
+//               <td>{clickCount}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
+
+// export default URLList;
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import useUpdateClickCount from './useUpdateClickCount'; // Adjust the path based on your project structure
+
+// const URLList = () => {
+//   const [urlList, setUrlList] = useState([]);
+
+//   useEffect(() => {
+//     const fetchUrlList = async () => {
+//       try {
+//         const response = await axios.get('https://url-shortener-ax8r.onrender.com/api/url-list');
+//         setUrlList(response.data);
+//       } catch (error) {
+//         console.error('Error fetching URL list:', error);
+//         // Handle error cases
+//       }
+//     };
+
+//     fetchUrlList();
+//   }, []);
+
+//   const { clickCount, handleUpdateClickCount } = useUpdateClickCount();
+
+//   const handleShortUrlClick = async (event, urlId, index) => {
+//     event.preventDefault();
+//     handleUpdateClickCount(urlId);
+//     // You can also handle the URL opening here if needed
+//   };
+
+//   return (
+//     <div className="url-list-container">
+//       <h2 className="url-list-heading">URL List</h2>
+//       <table className="url-list-table">
+//         <thead>
+//           <tr>
+//             <th>Short URL</th>
+//             <th>Long URL</th>
+//             <th>Click Count</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {urlList.map((url, index) => (
+//             <tr key={index}>
+//               <td>
+//                 <a href={url.shortURL} target="_blank" rel="noopener noreferrer" onClick={(e) => handleShortUrlClick(e, url._id, index)}>
+//                   {url.shortURL}
+//                 </a>
+//               </td>
+//               <td>
+//                 <div className="long-url-container">
+//                   <span className="long-url-text">{url.longURL}</span>
+//                 </div>
+//               </td>
+//               <td>{clickCount}</td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
+
+// export default URLList;
+
+
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import useUpdateClickCount from './useUpdateClickCount'; // Adjust the path based on your project structure
+import { FaTrash } from 'react-icons/fa'; // Import the delete icon
 
 const URLList = () => {
   const [urlList, setUrlList] = useState([]);
@@ -19,12 +153,19 @@ const URLList = () => {
     fetchUrlList();
   }, []);
 
-  const { clickCount, handleUpdateClickCount } = useUpdateClickCount();
+  const handleShortUrlClick = async (url) => {
+    window.open(url, '_blank'); // Open the link in a new tab without the localhost prefix
+  };
 
-  const handleShortUrlClick = async (event, urlId, index) => {
-    event.preventDefault();
-    handleUpdateClickCount(urlId);
-    // You can also handle the URL opening here if needed
+  const handleDeleteUrl = async (urlId) => {
+    console.log('URL ID to be deleted:', urlId);
+    try {
+      await axios.delete(`https://url-shortener-ax8r.onrender.com/api/urls/${urlId}`);
+      setUrlList((prevUrlList) => prevUrlList.filter((url) => url._id !== urlId));
+    } catch (error) {
+      console.error('Error deleting URL:', error);
+      // Handle error cases
+    }
   };
 
   return (
@@ -35,23 +176,27 @@ const URLList = () => {
           <tr>
             <th>Short URL</th>
             <th>Long URL</th>
-            <th>Click Count</th>
+            <th>Delete</th>
           </tr>
         </thead>
         <tbody>
           {urlList.map((url, index) => (
             <tr key={index}>
               <td>
-                <a href={url.shortURL} target="_blank" rel="noopener noreferrer" onClick={(e) => handleShortUrlClick(e, url._id, index)}>
+                <button onClick={() => handleShortUrlClick(url.shortURL)}>
                   {url.shortURL}
-                </a>
+                </button>
               </td>
               <td>
                 <div className="long-url-container">
                   <span className="long-url-text">{url.longURL}</span>
                 </div>
               </td>
-              <td>{clickCount}</td>
+              <td>
+                <button onClick={() => handleDeleteUrl(url._id)}>
+                  <FaTrash size={18} />
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
@@ -61,3 +206,5 @@ const URLList = () => {
 };
 
 export default URLList;
+
+
